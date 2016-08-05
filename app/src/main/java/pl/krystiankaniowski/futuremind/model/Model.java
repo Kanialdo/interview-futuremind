4921c85c69aa0053b2f5f57b11c6571b15e0ddd3
Krystian Kaniowski <kanialdo@gmail.com>
2016-08-05 23:25:41 +0200

    Add model (via http://www.jsonschema2pojo.org)

---
+++ app/src/main/java/pl/krystiankaniowski/futuremind/model/Model.java
@@ -0,0 +1,34 @@
+
+package pl.krystiankaniowski.futuremind.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+
+    @SerializedName("data")
+    @Expose
+    private List<Datum> data = new ArrayList<Datum>();
+
+    /**
+     * 
+     * @return
+     *     The data
+     */
+    public List<Datum> getData() {
+        return data;
+    }
+
+    /**
+     * 
+     * @param data
+     *     The data
+     */
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+}
